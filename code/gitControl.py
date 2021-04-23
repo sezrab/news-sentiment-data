8e31782153481df7ea81729b80ea48fd9f4164d0
samuelezraberry <samuel.ezra.berry@gmail.com>
2021-04-23 17:47:29 +0100

    Automatic update (gitControl.py)

--- code/gitControl.py
+++ code/gitControl.py
@@ -5,6 +5,6 @@ dir_path = os.path.dirname(os.path.realpath(__file__))
 def update():
     os.system("cd "+dir_path)  # ensure we are in the "code" directory
     os.system("git pull")  # ensure the local repo is up to date
-    os.system("git add *")  # stage changes
-    os.system("git push")  # push changes
+    os.system("git add .")  # stage changes
     os.system("git commit -m \"Automatic update (gitControl.py)\"")  # commit
+    os.system("git push")  # push changes
